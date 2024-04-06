432cf8b5a6f3c89ba1d2d96f8bd5d3d0c1597064
0xF76 <jakub.bubak03@gmail.com>
2024-04-06 08:27:52 +0200

    Implement some basic functions

--- src/CPS.jl
+++ src/CPS.jl
@@ -10,17 +10,17 @@ author = Dict{Symbol, String}(
 )
 
 # Sygnały ciągłe
-cw_rectangular(t::Real; T=1.0)::Real = missing
-cw_triangle(t::Real; T=1.0)::Real = missing
+cw_rectangular(t::Real; T=1.0)::Real = abs(t) < T/2 ? 1 : 0
+cw_triangle(t::Real; T=1.0)::Real = abs(t) < T/2 ? 1 - 1/(T/2)*abs(t) : 0
 cw_literka_M(t::Real; T=1.0)::Real = missing
 cw_literka_U(t::Real; T=1.0)::Real = missing
 
 ramp_wave(t::Real)::Real = missing
-sawtooth_wave(t::Real)::Real = missing
-triangular_wave(t::Real)::Real = missing
-square_wave(t::Real)::Real =  missing
-pulse_wave(t::Real, ρ::Real=0.2)::Real = missing
-impuse_repeater(g::Function, t1::Real, t2::Real)::Function = missing
+sawtooth_wave(t::Real)::Real = -(t-floor(t))
+triangular_wave(t::Real)::Real = 2*abs(t-floor(t+1/2))
+square_wave(t::Real)::Real =  sign(sin(2*π*t))
+pulse_wave(t::Real, ρ::Real=0.2)::Real = (t - floor(t)) ≤ ρ ? 1 : 0
+impulse_repeater(g::Function, t1::Real, t2::Real)::Function = missing
 
 function ramp_wave_bl(t; A=1.0, T=1.0, band=20.0)
     missing
@@ -55,8 +55,8 @@ end
 
 
 # Sygnały dyskretne
-kronecker(n::Integer)::Real = missing
-heaviside(n::Integer)::Real = missing
+kronecker(n::Integer)::Real = n == 0 ? 1 : 0
+heaviside(n::Integer)::Real = n ≥ 0 ? 1 : 0
 
 # Okna
 rect(N::Integer)::AbstractVector{<:Real} = missing
@@ -66,11 +66,11 @@ hamming(N::Integer)::AbstractVector{<:Real} = missing
 blackman(N::Integer)::AbstractVector{<:Real} = missing
 
 # Parametry sygnałów
-mean(x::AbstractVector)::Number = missing
-peak2peak(x::AbstractVector)::Real = missing
-energy(x::AbstractVector)::Real = missing
-power(x::AbstractVector)::Real = missing
-rms(x::AbstractVector)::Real = missing
+mean(x::AbstractVector)::Number = sum(x)/length(x)
+peak2peak(x::AbstractVector)::Real = maximum(x) - minimum(x)
+energy(x::AbstractVector)::Real = sum(x.^2)
+power(x::AbstractVector)::Real = energy(x)/length(x)
+rms(x::AbstractVector)::Real = √power(x)
 
 function running_mean(x::AbstractVector, M::Integer)::Vector
     missing
@@ -98,7 +98,7 @@ end
 # Kwantyzacja
 quantize(x::Real; L::AbstractVector)::Real = missing
 SQNR(N::Integer)::Real = missing
-SNR(Psignal, Pnoise)::Real = missing
+SNR(Psignal, Pnoise)::Real = Psignal/Pnoise
 
 
 # Obliczanie DFT
