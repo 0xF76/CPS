4821b0116698ec1214cc22218033f56a230d1a44
0xF76 <jakub.bubak03@gmail.com>
2024-05-28 23:52:55 +0200

    some fixes

--- src/CPS.jl
+++ src/CPS.jl
@@ -13,14 +13,14 @@ author = Dict{Symbol, String}(
 cw_rectangular(t::Real; T=1.0)::Real = abs(t) < T/2 ? 1 : 0
 cw_triangle(t::Real; T=1.0)::Real = abs(t) < T ? 1 - 1/T*abs(t) : 0
 cw_literka_M(t::Real; T=1.0)::Real = abs(t) < T/2 ? abs(t) : 0
-cw_literka_U(t::Real; T=1.0)::Real = missing
+cw_literka_U(t::Real; T=1.0)::Real = abs(t) < T/2 ? t^2 : 0
 
 ramp_wave(t::Real)::Real = t - floor(t)
-sawtooth_wave(t::Real)::Real = -(t-floor(t))
-triangular_wave(t::Real)::Real = 4*abs(t-floor(t+1/2))
+sawtooth_wave(t::Real)::Real = -2*(t-floor(t+1/2))
+triangular_wave(t::Real)::Real = 4*abs((t+1/4)-floor((t+1/4)+1/2)) - 1
 square_wave(t::Real)::Real =  sign(sin(2*π*t))
 pulse_wave(t::Real, ρ::Real=0.2)::Real = (t - floor(t)) ≤ ρ ? 1 : 0
-impulse_repeater(g::Function, t1::Real, t2::Real)::Function = x -> g(mod(x - t1, t2 - t1) + t1)
+impulse_repeater(g::Function, t1::Real, t2::Real)::Function = x -> g(mod(x - t1, t2 - t1))
 
 function ramp_wave_bl(t; A=1.0, T=1.0, band=20.0)
     sum = 0
@@ -29,7 +29,7 @@ function ramp_wave_bl(t; A=1.0, T=1.0, band=20.0)
         sum += (-1)^k * sin(arg * t) / k
         k += 1
     end
-    return -2 * A / π * um
+    return -2 * A / π * sum
 end
 
 function sawtooth_wave_bl(t; A=1.0, T=1.0, band=20.0)
@@ -43,15 +43,37 @@ function sawtooth_wave_bl(t; A=1.0, T=1.0, band=20.0)
 end
 
 function triangular_wave_bl(t; A=1.0, T=1.0, band=20.0)
-    missing
+    sum = 0
+    k = 1
+    while (arg = (2π*(2*k-1))) ≤ band * 2π
+        sum += ((-1)^k)/(2k-1)^2 * sin(arg*t)
+        k+=1
+    end
+    
+    return -8 / π^2 * sum
 end
 
 function square_wave_bl(t; A=1.0, T=1.0, band=20.0)
-    missing
+    sum = 0
+    k = 1
+    while (arg = (2π*(2*k-1))) ≤ band * 2π
+        sum += 1/(2*k-1) * sin(arg*t)
+        k+=1
+    end
+
+    return 4/π * sum
 end
 
 function pulse_wave_bl(t; ρ=0.2, A=1.0, T=1.0, band=20.0)
-    missing
+    sum = 0
+    k = 1
+    while (arg = 2*π*k/T) ≤ band * 2π
+        sum += 1/k * sin(π*k*ρ/T)*cos(arg*t)
+        k+=1
+    end
+
+    return A*ρ/T + 2*A/π * sum
+
 end
 
 
@@ -64,12 +86,12 @@ function rand_signal_bl(f1::Real, f2::Real)::Function
 end
 
 
-
-
 # Sygnały dyskretne
 kronecker(n::Integer)::Real = n == 0 ? 1 : 0
 heaviside(n::Integer)::Real = n ≥ 0 ? 1 : 0
 
+
+#TODO: DO POPRAWY
 # Okna
 rect(N::Integer)::AbstractVector{<:Real} = ones(N)
 triang(N::Integer)::AbstractVector{<:Real} = [1 - (2abs(n - ((N - 1) / 2))) / (N - 1) for n = 0:N-1]
@@ -103,7 +125,7 @@ function interpolate(
     m::AbstractVector,
     s::AbstractVector,
     kernel::Function=sinc
-)::Real
+)
     missing
 end
 
@@ -111,7 +133,6 @@ end
 quantize(x::Real; L::AbstractVector)::Real = missing
 SQNR(N::Integer)::Real = 1.76 + 6.02*N # 6.02*N [dB] is also correct
 SNR(Psignal, Pnoise)::Real = 10*log10(Psignal/Pnoise)
-
 
 # Obliczanie DFT
 function dtft(f::Real; signal::AbstractVector, fs::Real)
@@ -153,7 +174,7 @@ function ifft_radix2_dif_r(X::AbstractVector)::Vector
    missing
 end
 
-# TODO: THIS IS ABSOLUTELY NOT THE BEST WAY TO DO IT, BUT IT WORKS
+# THIS IS ABSOLUTELY NOT THE BEST WAY TO DO IT, BUT IT WORKS
 function fft(x::AbstractVector)::Vector
     if length(x) ≤ 1
         return x
@@ -170,13 +191,18 @@ function ifft(X::AbstractVector)::Vector
     idft(X) # Może da rade lepiej?
 end
 
+
 fftfreq(N::Integer, fs::Real)::Vector = missing
 rfftfreq(N::Integer, fs::Real)::Vector = missing
 amplitude_spectrum(x::AbstractVector, w::AbstractVector=rect(length(x)))::Vector = missing
 power_spectrum(x::AbstractVector, w::AbstractVector=rect(length(x)))::Vector = missing
 psd(x::AbstractVector, w::AbstractVector=rect(length(x)), fs::Real=1.0)::Vector = missing
 
-function periodogram(x::AbstractVector, w::AbstractVector=rect(length(x)), fs::Real=1.0)::Vector
+function periodogram(
+    x::AbstractVector,
+    w::AbstractVector=rect(length(x)),
+    L::Integer = 0,
+    fs::Real=1.0)::Vector
     missing
 end
 
@@ -192,12 +218,19 @@ function istft(X::AbstractMatrix{<:Complex}, w::AbstractVector{<:Real}, L::Integ
 end
 
 function conv(f::Vector, g::Vector)::Vector
-    y = zeros(length(f)+length(g)-1)
-    for m = 1:length(f)
-        for n = 1:length(g)
-            y[m+n-1] += f[m]*g[n]
+    N = length(f)
+    M = length(g)
+    K = N + M - 1
+    y = zeros(K)
+    for n = 1:K
+        for m = 1:M
+            if(n-m+1 > 0) && (n-m+1 <= M)
+                y[n] += f[m]*g[n-m+1]
+            end
         end
     end
+
+    return y
 end
 
 function fast_conv(f::Vector, g::Vector)::Vector
@@ -237,5 +270,51 @@ function lti_filter(b::Vector, a::Vector, x::Vector)::Vector
     missing
 end
 
+function filtfilt(b::Vector, a::Vector, x::Vector)::Vector
+    missing
 end
 
+function lti_amp(f::Real, b::Vector, a::Vector)::Real
+    missing
+end
+
+function lti_phase(f::Real, b::Vector, a::Vector)::Real
+    missing
+end
+
+
+function firwin_lp_I(order, F0)
+    missing
+end
+
+function firwin_hp_I(order, F0)
+    missing
+end
+
+function firwin_bp_I(order, F1, F2)
+    missing
+end
+
+function firwin_bs_I(order, F1, F2)
+    missing
+end
+
+function firwin_lp_II(N, F0)
+    missing
+end
+
+function firwin_bp_II(N, F1, F2)
+    missing
+end
+
+function firwin_diff(N::Int)
+    missing
+end
+
+function resample(x::Vector, M::Int, N::Int)
+    missing
+end
+
+
+
+end
